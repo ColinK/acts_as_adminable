c9f9b50c93bbd047205ad0f2a0b4b1bcde92a4f0
colin <colin@killswitchcollective.com>
2009-01-23 14:49:35 -0600

    now allows controller to decide whether to render the admin controls

--- init.rb
+++ init.rb
@@ -3,10 +3,8 @@
 #require 'action_controller/routing'
 require_library_or_gem "bluecloth"
 
-#This one saves us from having double-entries in the globalize_translations table for every phrase that gets auto-translated
-#Globalize::DbViewTranslator.send :include, Google::LocalizeCacheAccess
-
+ActionController::Base.send :include, ActsAsAdminable::ActionControllerExtension
+ActionView::Base.send :include, ActsAsAdminable::Helper
 ActionController::Routing::RouteSet::Mapper.send :include, ActsAsAdminable::MapperExtensions
-ActionView::Helpers::TagHelper.send :include, ActsAsAdminable::Helper
 
 #String.send :include, Google::String
--- lib/acts_as_adminable.rb
+++ lib/acts_as_adminable.rb
@@ -3,10 +3,29 @@
 module ActsAsAdminable
   DEFAULT_STRING = 'Click to Edit'
 
+  module ActionControllerExtension
+    def self.included(base)
+      base.extend ClassMethods
+    end
+    module ClassMethods
+      def acts_as_adminable(options = {})
+        RAILS_DEFAULT_LOGGER.debug "Got here, options = #{options.inspect}"
+        @is_adminable = options.has_key?(:if) ? options[:if].call : true
+        self.class_eval "def is_adminable?; #{@is_adminable.to_s}; end"
+      end
+#      def is_adminable?
+#        @is_adminable
+#      end
+    end
+  end
+
   module Helper
     def self.included(base)
       base.send :alias_method_chain, :content_tag, :adminable
-    end    
+    end
+    def is_adminable?
+      controller.is_adminable?
+    end
     def content_tag_with_adminable(name, content_or_options_with_block = nil, options = nil, escape = true, &block)
       if block_given?
         return content_tag_without_adminable(name, content_or_options_with_block, options, escape, &block)
@@ -25,27 +44,36 @@ module ActsAsAdminable
                         (db_contents["value"] || content_or_options_with_block || ActsAsAdminable::DEFAULT_STRING) : 
                         (content_or_options_with_block || ActsAsAdminable::DEFAULT_STRING)
                         
-        html_contents = markdown(auto_link(db_value)).gsub(/^<p>(.*)<\/p>$/,'\1') || content_or_options_with_block
+        html_contents = markdown(db_value).gsub(/^<p>(.*)<\/p>$/,'\1').gsub(/  /,'&nbsp;&nbsp;') || content_or_options_with_block
 
-        if true #TODO: is_admin?
+        if is_adminable?
           
           options[:onmouseover]='window.temp_style_background=this.style.backgroundColor; this.style.backgroundColor = "#FFD";'
           options[:onmouseout]='this.style.backgroundColor=window.temp_style_background; window.temp_style_background = undefined;'
-          options[:onclick]="s=this; f=$('#{key}_form'); t=$('#{key}_field'); p=s.cumulativeOffset(); d=s.getDimensions(); t.style.width=d.width+'px'; t.style.height=d.height+'px'; window.style_display=s.style.display; s.style.visibility='hidden'; f.style.display='block'; f.style.position='absolute'; f.style.left=s[0]; f.style.top=s[1];"
+          options[:onclick]="s=this; f=$('#{key}_form'); t=$('#{key}_field'); p=s.cumulativeOffset();  f.style.position='absolute'; f.style.left=p[0]+'px'; f.style.top=p[1]+'px'; d=s.getDimensions(); t.style.width=d.width+'px'; t.style.height=d.height+'px'; window.style_display=s.style.display; s.style.visibility='hidden'; f.style.display='block';"
           options[:style] = (options[:style] || options['style']).to_s + '; cursor: pointer'
+          
+          #which tags get edited by an INPUT, and which by a TEXTAREA
+          if [:h1, :h2, :h3, :h4, :h5, :h6, :span, :b, :i, :u, :em, :strong].include?(name)
+            text_input_html = text_field_tag(:value, (db_contents.nil? ? '' : db_contents["value"]),  :id => key+'_field')
+          elsif [:div, :td, :th, :pre, :p, :li, :blockquote].include?(name)
+            text_input_html = text_area_tag(:value, (db_contents.nil? ? '' : db_contents["value"]),  :id => key+'_field')
+          else
+            text_input_html = text_area_tag(:value, (db_contents.nil? ? '' : db_contents["value"]),  :id => key+'_field')
+          end
+          
+          #For some reason, this block prints immediately.  So no need to concatenate with our return value or anything like that
           form_remote_tag :url => '/admin/save_adminable_text', :html => {:style => 'display: none;', :id => key+'_form'} do
             hidden_field_tag(:content_key, key)     +
             hidden_field_tag(:object_id, object_id) +
-            text_field_tag(:value, (db_contents.nil? ? '' : db_contents["value"]),  :id => key+'_field') +
+            text_input_html +
             submit_tag('Save Text', :style => 'position: absolute; right: 0px; bottom: -25px;') +
             button_to_function('Cancel',"$('#{key}_form').style.display='none'; $('#{key}').style.visibility='visible'", :style => 'position: absolute; right: 100px; bottom: -25px;')
           end
-          
-          return content_tag_without_adminable(name, html_contents, options, escape)
 
-        else
-          return content_tag_without_adminable(name, html_contents, options, escape)
         end
+        
+        return content_tag_without_adminable(name, html_contents, options, escape)
 
       else
         return content_tag_without_adminable(name, content_or_options_with_block, options, escape)
@@ -78,7 +106,7 @@ module ActsAsAdminable
         sql = %Q{INSERT INTO adminable_text(content_key, data_type, value) VALUES ('#{key}', 'text', \"#{value}\") ON DUPLICATE KEY UPDATE value = \"#{value}\"}
         ActiveRecord::Base.connection.execute(sql) unless key.blank?  
         render :update do |page|
-          page.replace_html key, markdown(auto_link(params[:value]))[3..-5]
+          page.replace_html key, markdown(params[:value]).gsub(/^<p>(.*)<\/p>$/,'\1').gsub(/  /,'&nbsp;&nbsp;')
           page << "$('#{key}_form').style.display='none'"
           page << "$('#{key}').style.visibility='visible'"
         end
